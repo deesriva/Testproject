86d719b6ba7d7b45853c785b5352713da38b841b
DEESRIVA <DEESRIVA@DEESRIVA-PC>
2017-12-25 22:52:01 +0530

    Third Commit

--- 11/src/test/java/com/test/FirstTest.java
+++ 11/src/test/java/com/test/FirstTest.java
@@ -1,6 +1,7 @@
 package com.test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -15,7 +16,7 @@ public class FirstTest {
 	String driverPath = "..\\11\\";
 	
 	@Test
-	public void tc_1()
+	public void tc_1() throws InterruptedException
 	{
 
 		System.setProperty("webdriver.gecko.driver", driverPath+"geckodriver.exe");
@@ -24,6 +25,13 @@ public class FirstTest {
 		driver.get("http://www.google.com");
 		
 		driver.findElement(By.name("q")).sendKeys("selenium");
+		
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		
+		Thread.sleep(3000l);
+		driver.findElement(By.linkText("Downloads - Selenium")).click();
+		
+		System.out.println("Test passed");
 		
 	}
 	
